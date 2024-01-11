7797749390e844b2be55e8acc0693a172155c0a1
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-01-11 02:10:24 +0200

    simplfied clipboard

--- clipboard_interacter/clipboard.py
+++ clipboard_interacter/clipboard.py
@@ -1,51 +1,22 @@
+import time
 import pyperclip
-from pynput import keyboard as keyboard_listener
 
 
-class Clipboard:
+class ClipboardMonitor:
     def __init__(self):
-        self.current_clipboard_state = ""
-        self.listener = None
+        self.previous_clipboard_data = pyperclip.paste()
 
-    def get_clipboard_state(self):
-        return pyperclip.paste()
+    def check_clipboard_change(self):
+        current_clipboard_data = pyperclip.paste()
+        if current_clipboard_data != self.previous_clipboard_data:
+            print("Clipboard change detected: {}".format(current_clipboard_data))
+            self.previous_clipboard_data = current_clipboard_data
 
-    def set_clipboard_state(self, state):
-        pyperclip.copy(state)
-
-    def are_keys_pressed(self, keys):
-        current_keys = set(keys)
-        return current_keys.issubset(self.current_keys)
-
-    def on_press(self, key):
-        try:
-            current_key = key.char
-        except AttributeError:
-            current_key = str(key)
-
-        self.current_keys.add(current_key)
-
-        if self.are_keys_pressed(['ctrl', 'c']):
-            self.send_request()
-
-    def on_release(self, key):
-        try:
-            current_key = key.char
-        except AttributeError:
-            current_key = str(key)
-
-        self.current_keys.discard(current_key)
-
-    def set_event_listener(self):
-        with keyboard_listener.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
-            self.listener = listener
-            listener.join()
-
-    def send_request(self):
-        clipboard_content = self.get_clipboard_state()
-        print("Clipboard content:", clipboard_content)
+    def monitor_clipboard(self, interval_seconds):
+        while True:
+            self.check_clipboard_change()
+            time.sleep(interval_seconds)
 
 
-# Example Usage:
-clipboard_instance = Clipboard()
-clipboard_instance.set_event_listener()
+clipboard_monitor = ClipboardMonitor()
+clipboard_monitor.monitor_clipboard(interval_seconds=1)
